6581ed3fe2db23177c164a9d9084e44a51eb628d
Ben Coveney <bencoveney@gmail.com>
2025-09-18 00:08:26 +0100

    Configuring components

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,32 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface PageComponentPhoto extends Struct.ComponentSchema {
+  collectionName: 'components_page_component_photos';
+  info: {
+    displayName: 'Photo';
+  };
+  attributes: {
+    Description: Schema.Attribute.Text;
+    Image: Schema.Attribute.Media<'images' | 'files'> &
+      Schema.Attribute.Required;
+  };
+}
+
+export interface PageComponentText extends Struct.ComponentSchema {
+  collectionName: 'components_page_component_texts';
+  info: {
+    displayName: 'Text';
+  };
+  attributes: {
+    Text: Schema.Attribute.Blocks;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'page-component.photo': PageComponentPhoto;
+      'page-component.text': PageComponentText;
+    }
+  }
+}
